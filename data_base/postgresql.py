fc6a53088cd2eae6e3defaae654255d700890e1e
muhammedbadsha <badshanasar123@gmail.com>
2023-12-09 20:53:10 +0530

    initial setups 1

--- data_base/postgresql.py
+++ data_base/postgresql.py
@@ -1,5 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from decouple import config
 
 
+
+DB_NAME = config('DB_NAME')
+engine_psql = create_engine(DB_NAME)
+
+SessionLocal = sessionmaker(auto_commit=True,auto_flush=True,bind=engine_psql)
+
+Base = declarative_base()
